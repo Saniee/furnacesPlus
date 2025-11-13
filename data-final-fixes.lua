c2c4d50e36882249b0c025d99ed48548ad0ea4b8
Saniee <asamsku10@gmail.com>
2025-11-13 14:06:29 +0100

    0.1.4
    - Fixed the mod not showing in the mod explorer
    - Python script now loads the version from info.json

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,17 +1,11 @@
 data.raw["locomotive"]["locomotive"].energy_source.fuel_categories = {"chemical","rocket"}
-data.raw["locomotive"]["locomotive"].energy_source.fuel_category = nil
 
 data.raw["furnace"]["stone-furnace"].energy_source.fuel_categories = {"chemical","rocket"}
-data.raw["furnace"]["stone-furnace"].energy_source.fuel_category = nil
 
 data.raw["furnace"]["steel-furnace"].energy_source.fuel_categories = {"chemical","rocket"}
-data.raw["furnace"]["steel-furnace"].energy_source.fuel_category = nil
 
 data.raw["boiler"]["boiler"].energy_source.fuel_categories = {"chemical","rocket"}
-data.raw["boiler"]["boiler"].energy_source.fuel_category = nil
 
 data.raw["inserter"]["burner-inserter"].energy_source.fuel_categories = {"chemical","rocket"}
-data.raw["inserter"]["burner-inserter"].energy_source.fuel_category = nil
 
 data.raw["car"]["car"].energy_source.fuel_categories = {"chemical","rocket"}
-data.raw["car"]["car"].energy_source.fuel_category = nil
--- zip_mod.py
+++ zip_mod.py
@@ -1,36 +1,40 @@
 import zipfile
+import json
 
-version = "v0.1.3"
+with open("info.json", mode="r", encoding="utf-8") as info_f:
+    mod_data = json.load(info_f)
 
-with zipfile.ZipFile(f"furnacesPlus_{version}.zip", "w", zipfile.ZIP_LZMA, True, 9) as zf:
+version = mod_data["version"]
+
+with zipfile.ZipFile(f"furnacesPlus_{version}.zip", "w", zipfile.ZIP_DEFLATED, True, 9) as zf:
     # Mod info, and pic.
-    zf.write("thumbnail.png", f"furnacesPlus_{version}/thumbnail.png")
-    zf.write("info.json", f"furnacesPlus_{version}/info.json")
-    zf.write("changelog.txt", f"furnacesPlus_{version}/changelog.txt")
+    zf.write("thumbnail.png", "furnacesPlus/thumbnail.png")
+    zf.write("info.json", "furnacesPlus/info.json")
+    zf.write("changelog.txt", "furnacesPlus/changelog.txt")
     # Main Mod files.
-    zf.write("data.lua", f"furnacesPlus_{version}/data.lua")
-    zf.write("data-final-fixes.lua", f"furnacesPlus_{version}/data-final-fixes.lua")
-    zf.write("control.lua", f"furnacesPlus_{version}/control.lua")
+    zf.write("data.lua", "furnacesPlus/data.lua")
+    zf.write("data-final-fixes.lua", "furnacesPlus/data-final-fixes.lua")
+    zf.write("control.lua", "furnacesPlus/control.lua")
     # Sounds
-    zf.write("sounds/mk4-furnace.ogg", f"furnacesPlus_{version}/sounds/mk4-furnace.ogg")
-    zf.write("sounds/mk5-furnace.ogg", f"furnacesPlus_{version}/sounds/mk5-furnace.ogg")
+    zf.write("sounds/mk4-furnace.ogg", "furnacesPlus/sounds/mk4-furnace.ogg")
+    zf.write("sounds/mk5-furnace.ogg", "furnacesPlus/sounds/mk5-furnace.ogg")
     # Prototypes
-    zf.write("prototypes/furnace-mk1.lua", f"furnacesPlus_{version}/prototypes/furnace-mk1.lua")
-    zf.write("prototypes/furnace-mk2.lua", f"furnacesPlus_{version}/prototypes/furnace-mk2.lua")
-    zf.write("prototypes/furnace-mk3.lua", f"furnacesPlus_{version}/prototypes/furnace-mk3.lua")
-    zf.write("prototypes/furnace-mk4.lua", f"furnacesPlus_{version}/prototypes/furnace-mk4.lua")
-    zf.write("prototypes/furnace-mk5.lua", f"furnacesPlus_{version}/prototypes/furnace-mk5.lua")
+    zf.write("prototypes/furnace-mk1.lua", "furnacesPlus/prototypes/furnace-mk1.lua")
+    zf.write("prototypes/furnace-mk2.lua", "furnacesPlus/prototypes/furnace-mk2.lua")
+    zf.write("prototypes/furnace-mk3.lua", "furnacesPlus/prototypes/furnace-mk3.lua")
+    zf.write("prototypes/furnace-mk4.lua", "furnacesPlus/prototypes/furnace-mk4.lua")
+    zf.write("prototypes/furnace-mk5.lua", "furnacesPlus/prototypes/furnace-mk5.lua")
     # Locale
-    zf.write("locale/en/config.cfg", f"furnacesPlus_{version}/locale/en/config.cfg")
+    zf.write("locale/en/config.cfg", "furnacesPlus/locale/en/config.cfg")
     # Graphics
     #   Icons
-    zf.write("graphics/icons/electric-furnace.png", f"furnacesPlus_{version}/graphics/icons/electric-furnace.png")
-    zf.write("graphics/icons/mk4-furnace-icon.png", f"furnacesPlus_{version}/graphics/icons/mk4-furnace-icon.png")
-    zf.write("graphics/icons/mk5-furnace-icon.png", f"furnacesPlus_{version}/graphics/icons/mk5-furnace-icon.png")
-    zf.write("graphics/icons/steel-furnace.png", f"furnacesPlus_{version}/graphics/icons/steel-furnace.png")
-    zf.write("graphics/icons/stone-furnace.png", f"furnacesPlus_{version}/graphics/icons/stone-furnace.png")
+    zf.write("graphics/icons/electric-furnace.png", "furnacesPlus/graphics/icons/electric-furnace.png")
+    zf.write("graphics/icons/mk4-furnace-icon.png", "furnacesPlus/graphics/icons/mk4-furnace-icon.png")
+    zf.write("graphics/icons/mk5-furnace-icon.png", "furnacesPlus/graphics/icons/mk5-furnace-icon.png")
+    zf.write("graphics/icons/steel-furnace.png", "furnacesPlus/graphics/icons/steel-furnace.png")
+    zf.write("graphics/icons/stone-furnace.png", "furnacesPlus/graphics/icons/stone-furnace.png")
     #   Other
-    zf.write("graphics/mk4-furnace/mk4-furnace-anim.png", f"furnacesPlus_{version}/graphics/mk4-furnace/mk4-furnace-anim.png")
-    zf.write("graphics/mk4-furnace/mk4-furnace.png", f"furnacesPlus_{version}/graphics/mk4-furnace/mk4-furnace.png")
-    zf.write("graphics/mk5-furnace/mk5-furnace-anim.png", f"furnacesPlus_{version}/graphics/mk5-furnace/mk5-furnace-anim.png")
-    zf.write("graphics/mk5-furnace/mk5-furnace.png", f"furnacesPlus_{version}/graphics/mk5-furnace/mk5-furnace.png")
+    zf.write("graphics/mk4-furnace/mk4-furnace-anim.png", "furnacesPlus/graphics/mk4-furnace/mk4-furnace-anim.png")
+    zf.write("graphics/mk4-furnace/mk4-furnace.png", "furnacesPlus/graphics/mk4-furnace/mk4-furnace.png")
+    zf.write("graphics/mk5-furnace/mk5-furnace-anim.png", "furnacesPlus/graphics/mk5-furnace/mk5-furnace-anim.png")
+    zf.write("graphics/mk5-furnace/mk5-furnace.png", "furnacesPlus/graphics/mk5-furnace/mk5-furnace.png")
